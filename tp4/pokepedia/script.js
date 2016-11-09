8be59c6d836984c6795474d48bd8dc60c3cf8c3c
Humbert <moreaux.humbert@gmail.com>
2016-11-09 11:36:30 +0100

    add sprites

--- tp4/pokepedia/script.js
+++ tp4/pokepedia/script.js
@@ -91,11 +91,15 @@ function createIndexPokemonArticle(pokemon) {
 
 function createArticleSection(pokemon) {
     var section = document.createElement("section");
-    var aside = createCaracteristiqueAside(pokemon);
-    section.appendChild(aside);
+    var asideCaracteristiques = createCaracteristiqueAside(pokemon);
+    var asideSpritesEvolution = createSpritesEvolutionAside(pokemon);
+
+    section.appendChild(asideCaracteristiques);
+    section.appendChild(asideSpritesEvolution);
 
     return section;
 }
+
 function createCaracteristiqueAside(pokemon) {
     var aside = document.createElement("aside");
     aside.className = "aside-list";
@@ -108,17 +112,60 @@ function createCaracteristiqueAside(pokemon) {
     header.appendChild(h2);
     aside.appendChild(header);
 
-    console.log(pokemon['caracteristiques']);
     for (caracteristique in pokemon['caracteristiques']) {
         var li = addCaracteristiqueLiToUl(pokemon, caracteristique);
         ul.appendChild(li);
     }
-
-    // ul.appendChild(li);
     aside.appendChild(ul);
 
-
     return aside;
+}
+
+function createSpritesEvolutionAside(pokemon) {
+    // Ajout des éléments créés
+    var aside = document.createElement("aside");
+    aside.className = "aside-list";
+    aside.id = "sprites";
+
+    // création du header
+    var header = document.createElement("header");
+    var h2 = document.createElement("h2");
+    h2.innerHTML ="Evolution des sprites tout au long des générations";
+    header.appendChild(h2);
+
+    var ul = document.createElement("ul");
+
+    // création des sprite pour chaque génération
+    for (var sprite in pokemon.spritesByGeneration) {
+        ul.appendChild(createSpriteLi(pokemon, sprite));
+    }
+
+    aside.appendChild(ul);
+
+    return aside
+}
+
+function createSpriteLi(pokemon, sprite) {
+    // création de la colonne de gauche => Affichage du numéro de la génération
+    var divColLeft = document.createElement("div");
+    divColLeft.className = "col-left";
+    var h4 = document.createElement("h4");
+    h4.innerHTML = "Génération "+sprite+" :";
+    divColLeft.appendChild(h4);
+
+    // création de la colonne de droite => Affichage du sprite de la génération en cours
+    var divColRight = document.createElement("div");
+    divColRight.className = "col-right";
+    var img = document.createElement("img");
+    img.src = pokemon.spritesByGeneration[sprite];
+    divColRight.appendChild(img);
+
+    // ajout des colonnes à l'élément li
+    var li = document.createElement("li");
+    li.appendChild(divColLeft);
+    li.appendChild(divColRight);
+
+    return li;
 }
 
 function addCaracteristiqueLiToUl(pokemon, caracteristique) {
@@ -154,25 +201,6 @@ function addCaracteristiqueLiToUl(pokemon, caracteristique) {
     return li;
 }
 
-// function Pokemon(numero, nom, typePrincipal, description, image, famille, taille, poid, talents) {
-//     this.numero = numero;
-//     this.image = image;
-//     this.nom = nom;
-//     this.typePrincipal = typePrincipal;
-//     this.description = description;
-//     this.taille = taille;
-//     this.caracteristiques = {
-//         Famille: famille,
-//         Taille: taille,
-//         Poids: poid,
-//         Talents: talents
-//     }
-//
-//     this.getCaracteristiques = function () {
-//         return [this.caracteristiques];
-//     }
-// }
-
 function getPokemons() {
 
     var pokemons = [
@@ -187,6 +215,14 @@ function getPokemons() {
                 Taille: '0.7m',
                 Poids: '6.9kg',
                 Talents: ["Engrais", "Chlorophylle"]
+            },
+            spritesByGeneration: {
+                1: "http://www.pokepedia.fr/images/9/94/Sprite_1_j_001.png",
+                2: "http://www.pokepedia.fr/images/a/a4/Sprite_2_c_001.gif",
+                3: "http://www.pokepedia.fr/images/b/b2/Sprite_3_e_001.gif",
+                4: "http://www.pokepedia.fr/images/7/72/Sprite_4_o_001.png",
+                5: "http://www.pokepedia.fr/images/2/20/Sprite_5_n_001.png",
+                6: "http://www.pokepedia.fr/images/c/c2/Sprite_6_x_001.png"
             }
         },{
             numero: "002",
@@ -199,6 +235,14 @@ function getPokemons() {
                 Taille: "0.7m",
                 Poids: "6.9kg",
                 Talents: ["Engrais", "Chlorophylle"]
+            },
+            spritesByGeneration: {
+                1: "http://www.pokepedia.fr/images/9/94/Sprite_1_j_001.png",
+                2: "http://www.pokepedia.fr/images/a/a4/Sprite_2_c_001.gif",
+                3: "http://www.pokepedia.fr/images/b/b2/Sprite_3_e_001.gif",
+                4: "http://www.pokepedia.fr/images/7/72/Sprite_4_o_001.png",
+                5: "http://www.pokepedia.fr/images/2/20/Sprite_5_n_001.png",
+                6: "http://www.pokepedia.fr/images/c/c2/Sprite_6_x_001.png"
             }
         },{
             numero: "003",
@@ -211,6 +255,14 @@ function getPokemons() {
                 Taille: "0.7m",
                 Poids: "6.9kg",
                 Talents: ["Engrais", "Chlorophylle"]
+            },
+            spritesByGeneration: {
+                1: "http://www.pokepedia.fr/images/9/94/Sprite_1_j_001.png",
+                2: "http://www.pokepedia.fr/images/a/a4/Sprite_2_c_001.gif",
+                3: "http://www.pokepedia.fr/images/b/b2/Sprite_3_e_001.gif",
+                4: "http://www.pokepedia.fr/images/7/72/Sprite_4_o_001.png",
+                5: "http://www.pokepedia.fr/images/2/20/Sprite_5_n_001.png",
+                6: "http://www.pokepedia.fr/images/c/c2/Sprite_6_x_001.png"
             }
         },{
             numero: "004",
@@ -223,6 +275,14 @@ function getPokemons() {
                 Taille: "0.6m",
                 Poids: "8.5kg",
                 Talents: ["Brasier", "Force Soleil"]
+            },
+            spritesByGeneration: {
+                1: "http://www.pokepedia.fr/images/9/94/Sprite_1_j_001.png",
+                2: "http://www.pokepedia.fr/images/a/a4/Sprite_2_c_001.gif",
+                3: "http://www.pokepedia.fr/images/b/b2/Sprite_3_e_001.gif",
+                4: "http://www.pokepedia.fr/images/7/72/Sprite_4_o_001.png",
+                5: "http://www.pokepedia.fr/images/2/20/Sprite_5_n_001.png",
+                6: "http://www.pokepedia.fr/images/c/c2/Sprite_6_x_001.png"
             }
         }
     ];
